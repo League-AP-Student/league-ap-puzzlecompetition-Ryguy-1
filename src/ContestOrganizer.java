ce4552cd7f009a9e8e4487e3a52fed8a36834032
Ryguy-1 <Ryguy-1@users.noreply.github.com>
2020-04-03 22:44:59 -0700

    working

--- src/ContestOrganizer.java
+++ src/ContestOrganizer.java
@@ -17,6 +17,9 @@ public class ContestOrganizer {
 	 * @param arr the array to be sorted.*/
 	public void sort(Contestant[] arr){
 		/* code goes here */
+		
+		//WHAT IS DIFFERENCE BETWEEN THIS AND sortAllRows???
+		
 	}
 	
 	/** Sorts each row of contestants into increasing order by score.
@@ -24,6 +27,10 @@ public class ContestOrganizer {
 	 * 				  in the rightmost column of row[k], 0 <= k < NUM_ROWS */
 	public void sortAllRows(){
 		/* code goes here */ 
+		
+		
+		//WHY SHOULD THEY BE SORTED LIKE THE EXAMPLE SAYS???????
+		
 	}
 
 	/** Returns name of contestant with highest score
@@ -33,8 +40,17 @@ public class ContestOrganizer {
 	 * - Only one contestant has the highest score.
 	 * @return name of contestant with highest score. */
 	public String findWinnerName(){
-		/* code goes here */
 		
-		return "";
+		Contestant t = contestants[0][0];
+		
+		for (int i = 0; i < contestants.length; i++) {
+			for (int j = 0; j < contestants[i].length; j++) {
+				if(contestants[i][j].getScore()>t.getScore()) {
+					t = contestants[i][j];
+				}
+			}
+		}
+		
+		return t.getName();
 	}
 }
